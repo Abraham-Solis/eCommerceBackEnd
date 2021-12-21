1db35e514463485b932410201888c87f5c8fd6ba
email <abe.solis2015@gmail.com>
2021-12-20 21:08:30 -0800

    Product Model

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 // import important parts of sequelize library
+const { type } = require('express/lib/response')
 const { Model, DataTypes, DECIMAL } = require('sequelize')
 // import our database connection from config.js
 const sequelize = require('../config/connection')
@@ -23,6 +24,17 @@ Product.init(
        type:DataTypes.DECIMAL,
       allowNull: false,
       validate: {isDecimal: true}
+    },
+    stock:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue: 10, 
+      validate: {isNumeric:true}
+      
+    },
+    category_id:{
+      type:DataTypes.INTEGER,
+      references:{model:"Category", key:'id'}
     }
 
   },
