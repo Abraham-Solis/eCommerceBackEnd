404a46ecf6f79076a76d39daebe440ecfa7a2e53
email <abe.solis2015@gmail.com>
2021-12-20 20:52:16 -0800

    Category Model

--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,4 @@
+const { type } = require('express/lib/response')
 const { Model, DataTypes } = require('sequelize')
 
 const sequelize = require('../config/connection.js')
@@ -7,6 +8,15 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id:{ type:DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    category_name:{
+      type:DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
